cfe3248ab1d8c09990bba0d7114e821d7f7803d5
Ferris <bin@edmodo.com>
2019-10-12 20:14:05 +0800

    111

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Modal } from 'antd'
 import style from './style'
 import { findProductsInPage } from './../../services/product-service'
 
@@ -10,6 +11,8 @@ class Home extends Component {
       products: []
     }
     this.loadList = this.loadList.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+    this.toggleModal = this.toggleModal.bind(this)
   }
 
   componentDidMount() {
@@ -24,8 +27,16 @@ class Home extends Component {
     })
   }
 
+  toggleModal(currentItem) {
+    this.setState({ currentItem })
+  }
+
+  closeModal() {
+    this.setState({ currentItem: undefined })
+  }
+
   render() {
-    const { products } = this.state
+    const { products, currentItem } = this.state
     console.log('products=', products)
     return <div className={`${style.home} uk-section uk-section-muted`}>
       <div className="uk-container uk-container-small">
@@ -33,7 +44,9 @@ class Home extends Component {
           {
             products.map((productItem, index) => {
               const { title, image, desc } = productItem
-              return <div className={index % 3 === 0 ? 'uk-first-column uk-grid-margin' : 'uk-grid-margin'}>
+              return <div
+                className={index % 3 === 0 ? 'uk-first-column uk-grid-margin' : 'uk-grid-margin'}
+                onClick={() => this.toggleModal(productItem)}>
                 <div className="uk-card uk-card-default uk-card-hover">
                   <div className="uk-card-media-top">
                     <img src={image} className="Img" style={{ height: 200 }} alt="" />
@@ -47,46 +60,19 @@ class Home extends Component {
               </div>
             })
           }
-          {/* <Modal
+          <Modal
             title={null}
-            visible
+            visible={!!currentItem}
             closable={false}
+            bodyStyle={{ padding: 0 }}
             footer={null}
-            onOk={this.handleOk}
-            onCancel={this.handleCancel}
+            onCancel={this.closeModal}
           >
-            <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
-              <div className="uk-child-width-1-2@m uk-grid-match" uk-grid id="gap">
-                <img data-src="Images/Indonesia.jpg" className="modelImg" alt="" uk-img />
-                <img data-src="Images/ImageJob.png" className="modelImg" alt="" uk-img />
-              </div>
-              <div className="uk-container uk-container-small modelClass">
-                <div className="uk-child-width-1-2@m uk-grid-match" uk-grid>
-                  <form>
-                    <label htmlFor="fname" className="jobLable">Product Title</label>
-                    <input type="text" id="fname" name="fname" placeholder="Product Title" />
-                    <label htmlFor="lname" className="jobLable">Price</label>
-                    <input type="text" id="lname" name="lname" placeholder="Price" />
-                    <label htmlFor="lname" className="jobLable">Other Price</label>
-                    <input type="text" id="lname" name="lname" placeholder="Enter offer Price" />
-                    <label htmlFor="lname" className="jobLable">Shipping Cost</label>
-                    <input type="text" id="lname" name="lname" placeholder="Enter Cost" />
-                    <label htmlFor="lname" className="jobLable">Inventory
-                    </label>
-                    <input type="text" id="lname" name="lname" />
-
-                    <label htmlFor="lname" className="jobLable">Description</label>
-                    <input type="text" id="lname" name="lname" placeholder="Enter Description for Product" />
-                  </form>
-
-                  <h6 className="jobContent">
-                    <a href="#"><span className="uk-margin-small-right plusIcon" uk-icon="icon: plus; ratio: 1" /></a>
-                    Have Variations to your product like size,color and more?</h6>
-                </div>
-              </div>
-              <button className="btn"><i className="fa fa-arrow-right" aria-hidden="true" /></button>
+            <div>
+              <img src="/images/1.jpg" className="modelImg" alt="" />
+              <img src="/images/2.jpg" className="modelImg" alt="" />
             </div>
-          </Modal> */}
+          </Modal>
         </div>
       </div>
     </div>
